12492bbc1f17857fff27340b8b7c7684c685d63c
Janade <jbsahw121@gmail.com>
2022-10-17 08:43:47 -0700

    commit3

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,13 +3,13 @@
 /**
  *main - starting point
  *
- *Returns: returns zero
+ *Returns: returns 0
  */
 
 int main(void)
 {
 
-int x = 0;
+short x = 0;
 
 while (x < 10)
 {
@@ -17,6 +17,6 @@ putchar(x);
 x++;
 }
 putchar('\n');
-return 0;
+return (0);
 }
 
