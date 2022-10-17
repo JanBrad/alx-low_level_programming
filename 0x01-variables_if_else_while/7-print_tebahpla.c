e8bfb5cdec0f0fd2608e95dcbbda81126e991149
Janade <jbsahw121@gmail.com>
2022-10-17 09:45:43 -0700

    commit1

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - starting point
+ *
+ *Returns: returns 0
+ */
+
+int main(void)
+{
+
+char x = 'z';
+
+while (x >= 'a')
+{
+putchar(x);
+x--;
+}
+putchar('\n');
+return (0);
+}
