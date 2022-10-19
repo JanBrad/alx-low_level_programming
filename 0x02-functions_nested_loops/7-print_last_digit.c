21572f833c5c03dbd7a08dd80a5296b89c9c9ea2
Janade <jbsahw121@gmail.com>
2022-10-19 08:40:17 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_last_digit - checks code
+ *
+ *Return: Returns the value of the last digit x
+ */
+
+int print_last_digit(int x)
+{
+
+int y = x % 10;
+
+return (y);
+return (0);
+}
