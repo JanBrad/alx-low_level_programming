9fff4826f004a9909e54e3285a61f505ad183d67
Janade <jbsahw121@gmail.com>
2022-10-19 09:15:11 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,27 +14,19 @@ int print_last_digit(int x)
 if (x >= 0)
 {
 
-int n = 0;
-
-while (n < 2)
-{
 int y = (x % 10) * 11;
 
 return (y);
-}
+
 }
 
 else if (x < 0)
 {
 
-int n = 0;
-
-while (n < 2)
-{
 int y = (x % (-10)) * 11;
 
 return (y);
-}
+
 }
 
 return (0);
