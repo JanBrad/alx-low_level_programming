04b2a09dc2f1ab9091ede7f08498f63d83a39c67
Janade <jbsahw121@gmail.com>
2022-10-19 04:10:08 -0700

    commit 1

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_isalpha - checks code
+ *
+ *@c: parameter value
+ *
+ * Return: returns 1 if letter. Returns 0 if otherwise
+ */
+
+int _isalpha(int c)
+{
+
+if (c >= 'a' && c <= 'z' || c >= 'A' && C <= 'Z')
+{
+return (1);
+}
+return (0);
+}
