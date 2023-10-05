f58938f374e7665feeb30f196445d488b37f0a9f
JanBrad <jbshaw121@gmail.com>
2023-10-05 12:28:20 -0700

    betty coding style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main() {
-    puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+/**
+ * main - main function
+ *
+ * Returns 0
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return 0;
 }
