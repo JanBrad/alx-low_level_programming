2b5ad5383251d1fa6d6fb4d0116c2007d41ec583
JanBrad <jbshaw121@gmail.com>
2023-10-05 13:01:46 -0700

    C file that displays size of various data types in bytes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Returns: Always 0 (Success)
+ */
+
+int main(void)
+{
+printf("sizeof a char: %zu byte(s)", sizeof(char));
+printf("sizeof an int: %zu byte(s)", sizeof(int));
+print("sizeof a long int: %zu byte(s)", sizeof(long int);
+printf("sizeof a long long int: %zu byte(s)", sizeof(long long int));
+printf("sizeof a float: %zu byte(s)", sizeof(float));
+
+return (0);
+}
