458f218e0a1d69875078886889807d5132cf89f5
JanBrad <jbshaw121@gmail.com>
2023-10-05 12:44:11 -0700

    printf outputting text

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: Always 0 (Succes)
+ */
+
+int main(void)
+{
+char new_string[] = "with proper grammar, but the outcome is a piece of art,";
+printf("%s", new_string);
+return (0);
+}
