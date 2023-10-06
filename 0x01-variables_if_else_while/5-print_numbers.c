8aa1ca13a06a7c1ade8c791afe20d348d60e773e
JanBrad <jbshaw121@gmail.com>
2023-10-06 09:37:23 -0700

    C code that print num 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/* more headers goes there */
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+int num;
+
+for (num = 0; num < 10; num++)
+{
+printf("%d", num);
+}
+putchar('\n');
+
+return (0);
+}
