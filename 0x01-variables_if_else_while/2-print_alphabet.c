fd41be4b66cff3982ed31a4b424bb4c94f45147f
JanBrad <jbshaw121@gmail.com>
2023-10-06 09:11:40 -0700

    fixed some bugs

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -13,10 +13,10 @@ int main(void)
 
 char lower_case;
 
-for (lower_case = 'a'; lower_case != 'z'; lower_case++)
+for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 {
 putchar(lower_case);
+}
 putchar('\n');
-}	
-return 0;
+return (0);
 }
