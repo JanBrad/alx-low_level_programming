ee5ce30d93f64e71202f9f9f3e7d1e8939b98958
JanBrad <jbshaw121@gmail.com>
2023-10-06 12:18:40 -0700

    C code that prints all possibe combos of single digit nums

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/* more headers goes there */
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+int num;
+
+for (num = 0; num < 10; num++)
+{
+putchar('0' + num);
+
+if (num != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+
+return (0);
+}
+
