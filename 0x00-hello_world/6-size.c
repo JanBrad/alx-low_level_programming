6a9bb8c09fea33e7f5ab2bf29f7568101eb1ab67
JanBrad <jbshaw121@gmail.com>
2023-10-09 08:45:57 -0700

    Removed character from documentation and added space between size and of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,16 +3,16 @@
 /**
  * main - main function
  *
- * Returns: Always 0(Success)
+ * Return: Always 0(Success)
  */
 
 int main(void)
 {
-printf("Sizeof a char: %zu byte(s)\n", sizeof(char));
-printf("Sizeof an int: %zu byte(s)\n", sizeof(int));
-printf("Sizeof a long int: %zu byte(s)\n", sizeof(long int));
-printf("Sizeof a long long int: %zu byte(s)\n", sizeof(long long int));
-printf("Sizeof a float: %zu byte(s)\n", sizeof(float));
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
+printf("Siz of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
 return (0);
 }
