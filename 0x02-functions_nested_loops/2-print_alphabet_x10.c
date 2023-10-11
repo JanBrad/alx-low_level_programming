f1fb9c1dcbd64805a2df97b6ec8fe3799fcec737
JanBrad <jbshaw121@gmail.com>
2023-10-11 03:00:53 -0700

    prints alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Outputs lowercase alphabet
+ *
+ * Return: Always void
+ */
+
+void print_alphabet_x10(void)
+{
+
+char alphBet[] = "abcdefghijklmnopqrstuvwxyz";
+
+int y =0;
+
+while (y <= 9)
+{
+int x = 0;
+
+while (x <= 26)
+{
+_putchar(alphBet[x]);
+x++;
+
+while (x == 26)
+{
+_putchar('\n');
+x++;
+}
+}
+y++;
+}
+}
