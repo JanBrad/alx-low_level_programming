e8d7ab6e529dff7a5cac82828e2b5c360bcdd987
JanBrad <jbshaw121@gmail.com>
2023-10-11 02:18:24 -0700

    prints alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - Access point of program
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+
+char alphBet[] = "abcdefghijklmnopqrstuvwxyz";
+
+int x = 0;
+
+while (x <= 25)
+{
+_putchar(x);
+x++;
+
+while (x == 25)
+{
+_putchar('\n');
+x++;
+}
+}
+
