a546da13574c551877b3bc4a99b7fbf4b89bd5f8
JanBrad <jbshaw121@gmail.com>
2023-10-12 03:27:38 -0700

    ncommitted items from last session

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _islower - checks if function is lowercase char
+ *
+ * Return: Always 0 (Not lowercase) or 1 (Lowercase)
+ */
+
+int _islower(int c)
+{
+char lwrcase[] = "abcdefghijklmnopqrstuvwxyz";
+char upprcase[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+int x = 0;
+
+while (lwrcase[x] != c)
+{
+
+if (upprcase[x] == c)
+{
+return (0);
+}
+x++;
+}
+
+while (upprcase[x] != c)
+{
+
+if (lwrcase[x] == c)
+{
+return (1);
+}
+
+else if (c != lwrcase[x] && c != upprcase[x])
+{
+return (1);
+}
+x++;
+}
+
