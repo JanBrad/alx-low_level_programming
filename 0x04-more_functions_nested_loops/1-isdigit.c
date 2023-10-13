bf2e6734760e5204ea2878cb18b8ba28bc4e096f
JanBrad <jbshaw121@gmail.com>
2023-10-13 06:52:47 -0700

    checks if chararcter is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks if a character is uppercase.
+ *
+ * @c: The character to be checked.
+ *
+ * Return: 1 if @c is a digit, 0 otherwise.
+ */
+
+int _isdigit(int c)
+{
+
+char num[] = "0123456789";
+
+int x = 0;
+
+while (num[x] >= c || num[x] <= c)
+{
+
+if (num[x] == c)
+{
+return (1)
+}
+x++;
+}
+return (0);
+}
+
+
+
