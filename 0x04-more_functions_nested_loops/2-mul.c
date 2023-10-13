e1b5c92bc018e6a328763d1967b68bcb6572ae02
JanBrad <jbshaw121@gmail.com>
2023-10-13 09:30:38 -0700

    fixing betty documentation

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * mul - multiplies twi integers
+ * mul - Multiplies two integers
  *
- * @a: integer to be multiplied with @b
- * @b: integer to be multiplied with @a
+ * @a: Integer to be multiplied with @b
+ * @b: Integer to be multiplied with @a
  *
- * return: Always p
+ * Return: The result of the multiplication
  */
 
 int mul(int a, int b)
