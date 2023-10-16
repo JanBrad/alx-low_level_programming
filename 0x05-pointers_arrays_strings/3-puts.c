801e6e68294b8dd9a78f199ec25fa09e93fd56c3
JanBrad <jbshaw121@gmail.com>
2023-10-16 08:23:58 -0700

    prints string to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a new line
+ *
+ * @str: pointer to a null-terminated string
+ *
+ * return: the string to stdout
+ */
+
+void _puts(char *str)
+{
+
+int x;
+
+x = 0;
+
+while (str[x] != '\0')
+{
+_putchar(str[x]);
+}
+putchar('\n');
+}
