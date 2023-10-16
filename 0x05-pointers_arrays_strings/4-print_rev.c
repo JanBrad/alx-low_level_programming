dffd24b81c0ba80ccdea7509b5dda2ada7b4e67f
JanBrad <jbshaw121@gmail.com>
2023-10-16 09:09:27 -0700

    output is a little short though

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string in reverse
+ *
+ * @s: pointer to a null-terminated string
+ *
+ * return: void
+ */
+
+void print_rev(char *s)
+{
+
+int y;
+
+y = 0;
+
+while (s[y] != '\0')
+{
+y++;
+}
+
+while (s[y] != '\0')
+{
+y++;
+}
+
+while (s[y] != s[0])
+{
+_putchar(s[y]);
+y--;
+}
+_putchar('\n');
+}
