f500c4d4ca8d3177c4f261a64e3cc56ebd017687
JanBrad <jbshaw121@gmail.com>
2023-10-16 03:27:43 -0700

    function that changes value of n outside of function it was declared in

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value it points to
+ *
+ * @*n - points to an integer
+ *
+ * return: void
+ */
+
+void reset_to_98(int *n)
+{
+
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+In this directory, I will work in tasks that will test my knowledge of pointers, arrays, strings.
