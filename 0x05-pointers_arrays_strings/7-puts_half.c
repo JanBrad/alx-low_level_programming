585d6be99138e5e814dcaa3a42ff2a21323abd2c
JanBrad <jbshaw121@gmail.com>
2023-10-17 11:32:20 -0700

    prints last half of a string literal

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * puts_half - prints second half of a string
+ *
+ * @str: pointer to a string literal
+ *
+ * return: void
+ */
+
+void puts_half(char *str)
+{
+
+int x;
+int y;
+int k;
+
+x = 0;
+
+while (str[x] != '\0')
+{
+x++;
+}
+
+x--;
+
+if (x % 2 == 0)
+{
+
+y = x / 2;
+while (y <= x)
+{
+
+_putchar(str[y]);
+y++;
+}
+}
+else if ( x % 2 == 1)
+{
+
+k = ((x - 1) / 2) + 1;
+while ( k <= x)
+{
+
+_putchar(str[k]);
+k++;
+}
+}
+_putchar('\n');
+}
