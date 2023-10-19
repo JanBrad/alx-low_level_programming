1647e8926df3db9d90385a9d5c8c2ce643793d1b
JanBrad <jbshaw121@gmail.com>
2023-10-19 13:50:19 -0700

    Trying to make betty happy

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,27 +7,28 @@
  *
  * Return: Pointer to the concatenated string.
  */
-
 char *_strcat(char *dest, char *src)
 {
+	int y;
+	int x;
 
-int y;
-int x;
+	x = 0;
+	y = 0;
 
-x = 0;
-y = 0;
+	while (dest[x] != '\0')
+	{
+		x++;
+	}
 
-while (dest[x] != '\0')
-{
-x++;
+	while (src[y] != '\0')
+	{
+		dest[x] = src[y];
+		x++;
+		y++;
+	}
+
+	dest[x] = '\0';
+
+	return (dest);
 }
 
-while (src[y] != '\0') {
-        dest[x] = src[y];
-        x++;
-        y++;
-    }
-dest[x] = '\0';
-
-return (dest);
-}
