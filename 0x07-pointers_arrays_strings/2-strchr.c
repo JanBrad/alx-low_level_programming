9bea7eb73bb12a448cadeb2bc0bdfb791bfb6fb3
JanBrad <jbshaw121@gmail.com>
2023-10-23 09:35:36 -0700

    this code returns pointer to a character in main

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#define NULL ((void *)0)
+
+/**
+ * _strchr - locates a character in a string
+ *
+ * @s: string to be searched for character
+ * @c: character to be searched in string
+ *
+ * return: pointer to s
+ */
+
+char *_strchr(char *s, char c)
+{
+
+int x;
+
+x = 0;
+
+while (s[x] != '\0')
+{
+
+if (s[x] == c)
+{
+return (s + x);
+}
+x++;
+}
+
+return (NULL);
+
+}
+
