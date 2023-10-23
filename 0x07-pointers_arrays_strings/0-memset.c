7b7ece0b79aeb5d395d8a1a4b1034f264bb6a721
JanBrad <jbshaw121@gmail.com>
2023-10-23 05:22:48 -0700

    function that fills memory of char array

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ *
+ * @s: pointer to a char to have memory filled
+ * @b: constant byte that will fill memory
+ * @n: first number if bytes that will fill memory of char pointed to by s
+ *
+ * return: a pointer to a char
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+unsigned int x;
+
+
+for (x = 0; x < n; x++)
+{
+s[x] = b;
+}
+
+return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+In this dir I will work on more arrays, pointers, and strings. This time pointers to pointer and 2 dimensional arrayd will also be considered.
