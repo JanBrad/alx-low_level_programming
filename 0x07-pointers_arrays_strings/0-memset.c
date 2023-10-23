ab15ae57548d084b00ecbebbf290d21011c2447f
JanBrad <jbshaw121@gmail.com>
2023-10-23 05:28:43 -0700

    betty docu issues

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _memset - fills memory with a constant byte
+ * *_memset - fills memory with a constant byte
  *
  * @s: pointer to a char to have memory filled
  * @b: constant byte that will fill memory
