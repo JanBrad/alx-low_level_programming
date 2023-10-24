aba2890b7fc45a1dab8c97cd490df586c4d44092
JanBrad <jbshaw121@gmail.com>
2023-10-24 09:18:43 -0700

    searches pointer to char for first occurance of particular byte

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#define NULL ((void *) 0)
+
+/**
+ * _strpbrk -  searches a string for any of a set of bytes
+ *
+ * @s: pointer to the string to be searched
+ * @accept: pointer to the set of bytes to be compared with the string
+ *
+ * return: pointer to the first occurance of byte that matches byter in accept
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+
+int x;
+int y;
+
+x = 0;
+
+while (s[x] != '\0')
+{
+
+y = 0;
+while (accept[y] != '\0')
+{
+
+if (s[x] == accept[y])
+{
+
+return (s + x);
+}
+y++;
+}
+x++;
+}
+
+return (NULL);
+}
