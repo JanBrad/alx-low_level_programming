42c7289d47c04e375c7ae913ea45b51feb01a4ed
JanBrad <jbshaw121@gmail.com>
2023-10-26 12:05:45 -0700

    returns the pwr of an int

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ *
+ * @x: integer to be multiplied with itself
+ * @y: power  of x ( number of times x will multiply itself)
+ *
+ * return: the power of x, or -1 incase of an error
+ */
+
+int _pow_recursion(int x, int y)
+{
+
+if (y == 0)
+{
+
+return (1);
+}
+
+if (y == 1 )
+{
+
+return (x);
+}
+
+if (y < 0)
+{
+
+return (-1);
+}
+
+return (x * _pow_recursion(x, y - 1));
+
+
+}
