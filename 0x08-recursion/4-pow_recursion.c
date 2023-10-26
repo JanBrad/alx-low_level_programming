73b9f78899ec4626180942ff83df1a1285f535ae
JanBrad <jbshaw121@gmail.com>
2023-10-26 12:14:03 -0700

    betty is relentless

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -6,7 +6,7 @@
  * @x: integer to be multiplied with itself
  * @y: power  of x ( number of times x will multiply itself)
  *
- * return: the result of x raised to the power of y, or -1 in case of an error
+ * return: the result of @x raised to the power of @y, or -1 in case of an error
  */
 
 int _pow_recursion(int x, int y)
