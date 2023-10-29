e73939e8ca90e390b2894037aa29e29b78388fd3
JanBrad <jbshaw121@gmail.com>
2023-10-29 11:51:20 -0700

    prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Access point of program
+ *
+ * @argc - number of arguments passed (unused)
+ * @argv - pointer to an array of pointers to arguments
+ *
+ *Return: Always 0 (success)
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+
+printf("%s %s", argv[0], "\n");
+
+return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+In this directory I will complete tasks that concern the use of argc and argv in C main functions.
