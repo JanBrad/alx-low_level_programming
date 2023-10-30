3bb8a73d33d31aa3a2b8955a948603cd39e5291e
JanBrad <jbshaw121@gmail.com>
2023-10-30 03:34:20 -0700

    multiplies 2 numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of program
+ * @argc: Number of arguments passed
+ * @argv: Pointer to an array of pointers to arguments
+ *
+ * Return: 0 (Success), otherwise 1 (Error)
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+
+int x;
+
+if (argv[1] == NULL || argv[0] == NULL)
+{
+
+printf("Error\n");
+
+return (1);
+}
+
+else
+{
+
+x = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", x);
+
+return (0);
+}
+
+}
